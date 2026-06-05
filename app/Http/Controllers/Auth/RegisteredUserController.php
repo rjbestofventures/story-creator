d1d671fa99d86528f4df4638db9ac15d95f40120
rjbestofventures <rj@bestofventures.com>
2026-06-05 23:10:51 +0800

    refactor: Remove unused Registered event from user registration process

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Auth;
 use App\Actions\SeedDemoStoriesForUser;
 use App\Http\Controllers\Controller;
 use App\Models\User;
-use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -43,8 +42,6 @@ class RegisteredUserController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
-
-        event(new Registered($user));
 
         Auth::login($user);
 
